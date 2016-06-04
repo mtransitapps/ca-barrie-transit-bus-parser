20f97814d601f65c0149445919969659a700deed
Mathieu Méa <mathieu.mea@gmail.com>
2016-06-04 12:13:44 -0400

    Compatibility with latest update.

--- src/org/mtransit/parser/ca_barrie_transit_bus/BarrieTransitBusAgencyTools.java
+++ src/org/mtransit/parser/ca_barrie_transit_bus/BarrieTransitBusAgencyTools.java
@@ -1,12 +1,19 @@
 package org.mtransit.parser.ca_barrie_transit_bus;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.mtransit.parser.CleanUtils;
 import org.mtransit.parser.DefaultAgencyTools;
+import org.mtransit.parser.Pair;
+import org.mtransit.parser.SplitUtils;
+import org.mtransit.parser.SplitUtils.RouteTripSpec;
 import org.mtransit.parser.Utils;
 import org.mtransit.parser.gtfs.data.GCalendar;
 import org.mtransit.parser.gtfs.data.GCalendarDate;
@@ -14,9 +21,12 @@ import org.mtransit.parser.gtfs.data.GRoute;
 import org.mtransit.parser.gtfs.data.GSpec;
 import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.gtfs.data.GTrip;
+import org.mtransit.parser.gtfs.data.GTripStop;
 import org.mtransit.parser.mt.data.MAgency;
+import org.mtransit.parser.mt.data.MDirectionType;
 import org.mtransit.parser.mt.data.MRoute;
 import org.mtransit.parser.mt.data.MTrip;
+import org.mtransit.parser.mt.data.MTripStop;
 
 // http://www.barrie.ca/Living/Getting%20Around/BarrieTransit/Pages/Barrie-GTFS.aspx
 // http://transit.cityofbarriesites.com/files/google_transit.zip
@@ -115,6 +125,7 @@ public class BarrieTransitBusAgencyTools extends DefaultAgencyTools {
 	private static final String COLOR_F58220 = "F58220";
 	private static final String COLOR_000000 = "000000";
 	private static final String COLOR_007236 = "007236";
+	private static final String COLOR_FFFF00 = "FFFF00";
 
 	@Override
 	public String getRouteColor(GRoute gRoute) {
@@ -131,16 +142,62 @@ public class BarrieTransitBusAgencyTools extends DefaultAgencyTools {
 		case 6: return COLOR_B2D235;
 		case 7: return COLOR_F58220;
 		case 8: return COLOR_000000;
+		case 11: return COLOR_FFFF00;
 		case 90: return COLOR_007236;
 		// @formatter:on
 		}
-		System.out.println("Unexpected route color " + gRoute);
+		System.out.printf("\nUnexpected route color %s!", gRoute);
 		System.exit(-1);
 		return null;
 	}
 
+	private static HashMap<Long, RouteTripSpec> ALL_ROUTE_TRIPS2;
+	static {
+		HashMap<Long, RouteTripSpec> map2 = new HashMap<Long, RouteTripSpec>();
+		map2.put(11l, new RouteTripSpec(11l, //
+				MDirectionType.EAST.intValue(), MTrip.HEADSIGN_TYPE_STRING, "Fenchurch Mnr", //
+				MDirectionType.WEST.intValue(), MTrip.HEADSIGN_TYPE_STRING, "Pk Pl") //
+				.addTripSort(MDirectionType.EAST.intValue(), //
+						Arrays.asList(new String[] { //
+						"1", "247", "775" //
+						})) //
+				.addTripSort(MDirectionType.WEST.intValue(), //
+						Arrays.asList(new String[] { //
+						"775", "160", "1" //
+						})) //
+				.compileBothTripSort());
+		ALL_ROUTE_TRIPS2 = map2;
+	}
+
+	@Override
+	public int compareEarly(long routeId, List<MTripStop> list1, List<MTripStop> list2, MTripStop ts1, MTripStop ts2, GStop ts1GStop, GStop ts2GStop) {
+		if (ALL_ROUTE_TRIPS2.containsKey(routeId)) {
+			return ALL_ROUTE_TRIPS2.get(routeId).compare(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop);
+		}
+		return super.compareEarly(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop);
+	}
+
+	@Override
+	public ArrayList<MTrip> splitTrip(MRoute mRoute, GTrip gTrip, GSpec gtfs) {
+		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
+			return ALL_ROUTE_TRIPS2.get(mRoute.getId()).getAllTrips();
+		}
+		return super.splitTrip(mRoute, gTrip, gtfs);
+	}
+
+	@Override
+	public Pair<Long[], Integer[]> splitTripStop(MRoute mRoute, GTrip gTrip, GTripStop gTripStop, ArrayList<MTrip> splitTrips, GSpec routeGTFS) {
+		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
+			return SplitUtils.splitTripStop(mRoute, gTrip, gTripStop, routeGTFS, ALL_ROUTE_TRIPS2.get(mRoute.getId()));
+		}
+		return super.splitTripStop(mRoute, gTrip, gTripStop, splitTrips, routeGTFS);
+	}
+
 	@Override
 	public void setTripHeadsign(MRoute mRoute, MTrip mTrip, GTrip gTrip, GSpec gtfs) {
+		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
+			return; // split
+		}
 		GRoute gRoute = gtfs.getRoute(gTrip.getRouteId());
 		String rsn = gRoute.getRouteShortName();
 		String rsn_letter = rsn.substring(rsn.length() - 1, rsn.length());
@@ -151,7 +208,7 @@ public class BarrieTransitBusAgencyTools extends DefaultAgencyTools {
 		} else if (B.equals(rsn_letter)) {
 			directionId = 1;
 		} else {
-			System.out.printf("Unexpected trip (unexpected rsn: %s): %s\n", rsn, gTrip);
+			System.out.printf("\nUnexpected trip (unexpected rsn: %s): %s", rsn, gTrip);
 			System.exit(-1);
 			directionId = -1;
 		}
@@ -164,13 +221,11 @@ public class BarrieTransitBusAgencyTools extends DefaultAgencyTools {
 		return CleanUtils.cleanLabel(tripHeadsign);
 	}
 
-	private static final Pattern AT = Pattern.compile("( at )", Pattern.CASE_INSENSITIVE);
 
-	private static final String AT_REPLACEMENT = " / ";
 
 	@Override
 	public String cleanStopName(String gStopName) {
-		gStopName = AT.matcher(gStopName).replaceAll(AT_REPLACEMENT);
+		gStopName = CleanUtils.CLEAN_AT.matcher(gStopName).replaceAll(CleanUtils.CLEAN_AT_REPLACEMENT);
 		gStopName = CleanUtils.cleanStreetTypes(gStopName);
 		return CleanUtils.cleanLabel(gStopName);
 	}
